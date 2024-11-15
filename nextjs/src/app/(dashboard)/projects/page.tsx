284ef4abfe31cdb6f4fd4d091faa2ae4b93969a2
maciker <ikermacaya@gmail.com>
2024-11-15 13:20:14 +0100

    dashboard group route created. Dummy components added to each route

---
+++ nextjs/src/app/(dashboard)/projects/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function ProjectsPage() {
+  const projects = [
+    { id: 1, name: "Project 1", description: "Description 1" },
+    { id: 2, name: "Project 2", description: "Description 2" },
+    { id: 3, name: "Project 3", description: "Description 3" },
+    { id: 4, name: "Project 4", description: "Description 4" },
+    { id: 5, name: "Project 5", description: "Description 5" },
+    { id: 6, name: "Project 6", description: "Description 6" },
+    { id: 7, name: "Project 7", description: "Description 7" },
+    { id: 8, name: "Project 8", description: "Description 8" },
+    { id: 9, name: "Project 9", description: "Description 9" },
+    { id: 10, name: "Project 10", description: "Description 10" },
+  ];
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>{project.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
