90b0e87227453ff5377bbcd4fdadf675361905ab
maciker <ikermacaya@gmail.com>
2024-11-15 13:33:31 +0100

    Routes and components added to project and template

---
+++ nextjs/src/app/(dashboard)/project/[projectId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface ProjectPageProps {
+  params: {
+    projectId: string;
+  };
+}
+
+export default function ProjectPage({ params }: ProjectPageProps) {
+  return <div>Project Page {params.projectId}</div>;
+}
---
+++ nextjs/src/app/(dashboard)/template/[templateId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface TemplatePageProps {
+  params: {
+    templateId: string;
+  };
+}
+
+export default function TemplatePage({ params }: TemplatePageProps) {
+  return <div>Template Page {params.templateId} </div>;
+}
