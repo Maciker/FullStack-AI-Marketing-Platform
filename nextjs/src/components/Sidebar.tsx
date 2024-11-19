88369fd712be487821b68e6c537ab7b65c53ea65
maciker <ikermacaya@gmail.com>
2024-11-19 12:10:40 +0100

    Sidebar: states added

--- nextjs/src/components/Sidebar.tsx
+++ nextjs/src/components/Sidebar.tsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from "react";
+
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  useEffect(() => {}, []);
+
   return <div className="w-28 bg-white text-black h-screen">Sidebar</div>;
 }
