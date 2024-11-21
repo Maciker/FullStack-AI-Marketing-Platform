704125dbe71e0bd3865f9987d1787450940a0624
maciker <ikermacaya@gmail.com>
2024-11-21 10:07:05 +0100

    Sidebar: SidebarNav component created

---
+++ nextjs/src/components/SidebarNav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface SidebarNavProps {
+  isMobile: boolean;
+  isCollapsed: boolean;
+}
+
+function SidebarNav({ isMobile, isCollapsed }: SidebarNavProps) {
+  return <div>SidebarNav</div>;
+}
+
+export default SidebarNav;
