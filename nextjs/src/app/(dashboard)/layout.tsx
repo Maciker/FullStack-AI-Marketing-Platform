cea9078ba8103861f9244ba349e62bb8e0d29e90
maciker <ikermacaya@gmail.com>
2024-11-21 11:04:44 +0100

    Sidebar: upadte layout

--- nextjs/src/app/(dashboard)/layout.tsx
+++ nextjs/src/app/(dashboard)/layout.tsx
@@ -2,16 +2,17 @@ import Sidebar from "@/components/Sidebar";
 
 export default function DashboardLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <div className="flex flex-col lg:flex-row min-h-screen bg-white">
       <Sidebar />
 
-      <main className="flex-1 p-4 sm:p-6 md:p-8 lg:p-6 pt-16 lg:pt-8 overflow-auto">
+      <main className="flex-1 max-h-screen p-4 sm:p-6 md:p-8 lg:p-6 pt-16 lg:pt-8 overflow-auto">
         {children}
       </main>
     </div>
   );
+}
 }
